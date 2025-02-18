25de2fac3d4909f0ce062013aab9c02a690649d7
Priyanjal Saxena <priyanjalsaxena1234@gmail.com>
2025-02-18 14:29:35 +0530

    Created the route and page for Email Verification

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
+import VerifyEmail from "./pages/VerifyEmail";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register/:name" element={<Register />} />
+        <Route path="/verify-email" element = {<VerifyEmail />}/>
       </Routes>
     </>
   );
--- src/components/StudentRegister.jsx
+++ src/components/StudentRegister.jsx
@@ -1,5 +1,5 @@
-import React, { useContext, useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import FormField from "./FormField";
 import SubmitBtn from "./SubmitBtn";
 import { BookOpen } from "lucide-react";
@@ -14,6 +14,7 @@ import { toast } from "react-toastify";
 
 const StudentRegister = () => {
   const { darkMode } = useContext(ThemeDataContext);
+  let navigate = useNavigate()
 
   const { values, errors, handleChange } = useFormHandler(
     { name: "", email: "", password: "",role: "student" },
@@ -25,9 +26,10 @@ const StudentRegister = () => {
 
     try {
       await userService.createAccount(values);
+      toast.success("Check Your Email For Email Verification")
+      navigate("/verify-email")
     } catch (error) {
-      console.log(error)
-      
+      console.log(error)    
     }
   }
 
