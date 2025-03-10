6b941208787a30903dda24f30193df6f1aa7fbe0
Priyanjal Saxena <priyanjalsaxena1234@gmail.com>
2025-03-10 20:56:14 +0530

    Added the condition for no notification

--- src/components/Notifications.jsx
+++ src/components/Notifications.jsx
@@ -9,7 +9,7 @@ const Notifications = () => {
         <div className='w-full h-screen text-white bg-[#101828] flex items-center justify-center'>
             <div className='px-3 py-2 bg-[#1E2939] rounded-lg w-fit'>
                 {
-                  Array.isArray(allNotifications) && allNotifications.length > 0 && allNotifications.map((notification,index) => (
+                  Array.isArray(allNotifications) && allNotifications.length === 0 ? "No Notifications" : allNotifications.map((notification,index) => (
                     <p>{notification?.message}</p>
                   ))
                 }
