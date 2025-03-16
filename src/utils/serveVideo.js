bae1a7d6bdfed0510a186d4e7783dd94427b1dea
Priyanjal Saxena <priyanjalsaxena1234@gmail.com>
2025-03-16 15:23:58 +0530

    Removed the presigned url logic from backend and allowed the read only access to aws bucket

--- src/utils/serveVideo.js
+++ src/utils/serveVideo.js
@@ -1,22 +1,6 @@
-import { GetObjectCommand } from '@aws-sdk/client-s3';
-import s3Client from '../config/awsConfig.js';
-import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
-
 async function getVideoUrl(key) {
-  const params = {
-    Bucket: 'lms-videos-bucket-in-aws',
-    Key: key,
-    Expires: 3600, 
-  };
-
-  try {
-    const command = new GetObjectCommand(params);
-    const url = await getSignedUrl(s3Client, command, { expiresIn: 3600 });
-    return url;
-  } catch (error) {
-    console.error('Error generating URL:', error);
-    throw error;
-  }
+  const url = `https://lms-videos-bucket-in-aws.s3.us-east-1.amazonaws.com/${key}`;
+  return url;
 }
 
-export default getVideoUrl
+export default getVideoUrl;
