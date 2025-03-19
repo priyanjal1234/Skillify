858b5b62d7b5b86409319b3c76a9fdb172d886e5
priyanjal1234 <priyanjal362@gmail.com>
2025-03-19 17:36:51 +0530

    Again changed the backend url

--- src/components/InstructorLogin.jsx
+++ src/components/InstructorLogin.jsx
@@ -50,7 +50,7 @@ const InstructorLogin = () => {
 
   async function handleGoogleLogin() {
     try {
-      window.open("https://skillify-lms.xyz/api/users/google", "_self");
+      window.open("https://skillify-backend.onrender.com/api/users/google", "_self");
       dispatch(setLoggedin(true))
     } catch (error) {
       toast.error(
