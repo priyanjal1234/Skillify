1345a3eb09cf5b6a72b51e437f2ccb9681197c56
priyanjal1234 <priyanjal362@gmail.com>
2025-03-19 12:07:45 +0530

    Update verifyOTPSchema.js

--- src/schemas/verifyOTPSchema.js
+++ src/schemas/verifyOTPSchema.js
@@ -1,1 +1,10 @@
+import { z } from "zod";
 
+const verifyOTPSchema = z.object({
+  email: z.string().email("Invalid email address"),
+  otp: z
+    .string()
+    .regex(/^\d{6}$/, "Verification code must be a 6-digit number"),
+});
+
+export default verifyOTPSchema;
