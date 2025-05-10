cca68f415a7315f60d3224778b8907987720ecc9
Priyanjal Saxena <priyanjalsaxena1234@gmail.com>
2025-05-10 19:56:57 +0530

    Changed the region in awsconfig.js

--- backend/src/config/awsConfig.js
+++ backend/src/config/awsConfig.js
@@ -2,7 +2,7 @@ import { S3Client } from '@aws-sdk/client-s3';
 import { awsAccessKeyId, awsSecretAccessKey } from '../constants.js';
 
 const s3Client = new S3Client({
-  region: 'us-east-1', 
+  region: 'ap-south-1', 
   credentials: {
     accessKeyId: awsAccessKeyId,
     secretAccessKey: awsSecretAccessKey,
