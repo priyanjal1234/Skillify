124b57f21763cf07fe3edf509ea9f21eb4fae5b7
Priyanjal Saxena <priyanjalsaxena1234@gmail.com>
2025-05-26 20:53:48 +0530

    Again changed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -149,7 +149,7 @@ const Navbar = () => {
               )}
             </button>
 
-            {isLoggedin === false && currentUser ? (
+            {isLoggedin === false && !currentUser ? (
               <>
                 {/* Sign Up Link */}
                 <Link
