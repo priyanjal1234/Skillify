1f3d4877a65c0d306b870ec0e7706b5b9586e68b
priyanjal1234 <priyanjal362@gmail.com>
2025-06-03 13:32:37 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,7 +14,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:5173'],
+    origin: ['https://skillify-frontend-prod.vercel.app','https://skillify-admin-dashboard.vercel.app'],
     credentials: true,
   },
 });
