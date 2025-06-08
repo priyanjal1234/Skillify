fa4c6244692cd28c04614f83d4fe16761644e604
Priyanjal Saxena <priyanjalsaxena1234@gmail.com>
2025-06-08 13:53:49 +0530

    I think solved

--- frontend/src/components/CourseCard.jsx
+++ frontend/src/components/CourseCard.jsx
@@ -162,9 +162,7 @@ const CourseCard = ({ course }) => {
 
         {/* Go to Classroom Button */}
         {course?.studentsEnrolled?.includes(currentUser?._id) &&
-        isLoggedin &&
-         currentOrder?.paymentStatus &&
-        currentOrder?.paymentStatus !== "Pending" ? (
+        isLoggedin ? (
           <Link
             to={`/classroom/${course?._id}`}
             className="w-full mt-4 bg-indigo-600 hover:bg-indigo-700 text-white font-medium py-2.5 px-4 rounded-xl flex items-center justify-center text-sm sm:text-base"
